126e9b9a2dcc9f15a370caa826d32e8da0f038ed
vsteffen <vsteffen@student.42.fr>
2018-10-01 17:29:08 +0200

    Push ex00

---
+++ d00/Assets/ex00/Scripts/Balloon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballon_inflate : MonoBehaviour {
+	// Vector3 theScale;
+
+	// Use this for initialization
+	void Start () {
+		// theScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+			transform.localScale += new Vector3(0.1F, 0.1f, 0);
+			Debug.Log(transform.localScale);
+        }	
+	}
+}
