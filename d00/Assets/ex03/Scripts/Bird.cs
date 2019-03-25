35b0f769a4e08ec7d9904b6033705b7873ced9f5
vsteffen <vsteffen@student.42.fr>
2019-03-25 20:36:24 +0100

    Working on d00/ex03

---
+++ d00/Assets/ex03/Scripts/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+	public float	speed;
+	public float	minPosY;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+			transform.Translate(Vector3.up * Time.deltaTime * speed);
+		else
+			transform.Translate(Vector3.down * Time.deltaTime * speed);
+		// if (transform.position.y)
+	}
+}
---
+++ d00/Assets/ex03/Scripts/Scroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+	public float 	speed;
+	public float	repeatPos;
+	private Vector3	initialPos;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// transform.position = new Vector3(speed, 0f, 0f);
+		transform.Translate(Vector3.left * Time.deltaTime * speed);
+		if (repeatPos > transform.position.x)
+			transform.position = initialPos;
+	}
+}
