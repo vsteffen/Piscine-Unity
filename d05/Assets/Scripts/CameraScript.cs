519b70f0e18b530fb99a0c34905019394df14010
Vivien STEFFEN <vsteffen@e2r4p17.42.fr>
2019-04-01 23:08:24 +0200

    beginning of d05

---
+++ d05/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	public GameObject to_follow;
+	public Vector3 _offset;
+
+	void Start()
+	{
+		// _offset = new Vector3(-50f, 50f, 10); // or whatever you need
+	}
+
+	void FixedUpdate()
+	{
+		transform.position = to_follow.transform.position + _offset;
+		transform.LookAt(to_follow.transform.position);
+	}
+}
